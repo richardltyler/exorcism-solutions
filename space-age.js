da0bb4b87270fe3e28e05b40a2f97a83056a407a
Richard Tyler <richardtyler2011@gmail.com>
2021-05-20 11:19:54 -0600

    Add space age

---
+++ space-age.js
@@ -0,0 +1,21 @@
+const planetsAge = {
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132
+};
+
+const secondsInEarthYear = 31_557_600;
+
+const calculateEarthAge = (age) => age / secondsInEarthYear;
+
+export const age = (planet, age) => {
+  const earthAge = calculateEarthAge(age);
+
+  return Math.round((earthAge / planetsAge[planet]) * 100) / 100;
+};
+
