176989f7484fd245d529e61581cfdbabb6a87013
Richard Tyler <richardtyler2011@gmail.com>
2021-05-20 11:18:27 -0600

    Add resistor color

---
+++ resistor-color.js
@@ -0,0 +1,16 @@
+const resistors = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9
+}
+
+export const colorCode = color => resistors[color];
+
+export const COLORS = Object.keys(resistors);
