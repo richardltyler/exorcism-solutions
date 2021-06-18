e58fdae243d267dc34f00af9d864f614bc807af3
Richard Tyler <richardtyler2011@gmail.com>
2021-06-18 09:11:22 -0600

    Refactor rna trans with reduce method

--- rna-transcription.js
+++ rna-transcription.js
@@ -14,3 +14,18 @@ export const toRna = (DNA = '') => {
   
   return newRNA;
 };
+
+// I've found a  solution using the array prototype method .reduce()
+// I've heard that .reduce() is difficut to read 
+// so use with caution 
+
+// export const toRna = (DNA = '') => {
+//   DNA = DNA.split('')
+
+//   return DNA.reduce((acc, nucleotide) => {
+  
+//     acc = acc + nucleotides[nucleotide];
+  
+//     return acc;
+//   }, '');
+// };
