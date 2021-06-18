83268f0c811d8d48d38a89639425cb2906d56837
Richard Tyler <richardtyler2011@gmail.com>
2021-06-18 09:04:26 -0600

    Add array solution to resistor color

--- resistor-color.js
+++ resistor-color.js
@@ -14,3 +14,22 @@ const resistors = {
 export const colorCode = color => resistors[color];
 
 export const COLORS = Object.keys(resistors);
+
+// I thought of a different and better way to do this
+// basically the resistors object above is an inverse of what an array really is so I used an actual array this time
+// it also cuts down on memory by assigning fewer variables
+
+// export const COLORS = [
+//   'black',
+//   'brown',
+//   'red',
+//   'orange',
+//   'yellow',
+//   'green',
+//   'blue',
+//   'violet',
+//   'grey',
+//   'white'
+// ];
+
+// export const colorCode = color => COLORS.indexOf(color);
